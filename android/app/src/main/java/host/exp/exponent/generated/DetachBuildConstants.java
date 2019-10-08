1cab0eee5c9bf41beefdba6713c8a4280df7822b
rafaelferreira_deway <rafael.ferreira@deway.com.br>
2019-10-08 14:40:47 -0300

    Initial commit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expbde9132a95544f429badf9fed2b91c27://tu-b3g.fael-f.issuebuild.exp.direct:80";
+
+}
